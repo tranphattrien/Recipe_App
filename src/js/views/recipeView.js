1a8781245b0d3be51e4870304a05221b6472b49d
Tran Phat Trien <trientranphat@gmail.com>
2023-02-20 00:16:01 +0700

    Fix Fraction

--- src/js/views/recipeView.js
+++ src/js/views/recipeView.js
@@ -1,5 +1,5 @@
 import View from "./View";
-import { Fraction } from "fractional";
+import fracty from "fracty";
 class RecipeView extends View {
   _parentElement = document.querySelector(".recipe");
   _message = "";
@@ -121,7 +121,7 @@ class RecipeView extends View {
         <i class="fa-solid fa-check"></i>
       </div>
       <div class="recipe__quantity">${
-        ing.quantity ? new Fraction(ing.quantity).toString() : ""
+        ing.quantity ? fracty(ing.quantity).toString() : ""
       }</div>
       <div class="recipe__description">
         <span class="recipe__unit">${ing.unit}</span>
